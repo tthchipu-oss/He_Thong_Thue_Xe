def0a6079cb3685627a5355e968ba4a317b4fff7
tthchipu-oss <tthchipu@gmail.com>
2026-05-28 10:52:12 +0700

    deploy 5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // 2. THÊM ĐOẠN CODE NÀY VÀO HÀM BOOT
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
